f5474e8c3b7a51584f199be1996e8ee8c918e541
Adela SN <adela.nadhifa14@gmail.com>
2023-12-03 08:17:37 +0700

    initail add

---
+++ TaskManagementSystem_ASP.Net_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskManagementSystem_ASP.Net_MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
